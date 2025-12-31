06bec7a7c7218a71fd3e45edbafd1e83ac2eb34b
mehmet2725 <mehmetsonmez2725@gmail.com>
2025-12-31 22:05:44 +0300

    Frontend Initialized: Vite + React + Bootstrap setup complete

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+function App(){
+  return (
+    <div className="containermt-5">
+      <div className="card text-center shadow">
+        <div className="card-header bg-dark text-white">
+          Sistem Kontrolü
+        </div>
+        <div className="card-body">
+          <h1 className="text-primary">Mayday Sipariş Modülü</h1>
+          <p className="lead">Frontend ve Bootstrap sorunsuz çalışıyor!</p>
+          <button className="btn btn-succes btn-lg">Siparişleri Getir</button>
+        </div>
+        <div className="card-footer text-muted">Hazırız!!</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
